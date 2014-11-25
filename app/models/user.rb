ad792c2e9e4dbb32fa964fbd8591a5298149d829
AlexKtf <alexandre.ktifa@gmail.com>
2014-11-25 10:37:15 +0100

    WIP

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  after_create :send_activation_admin
+
+  def send_activation_admin
+    send_reset_password_instructions   
+  end
 end
